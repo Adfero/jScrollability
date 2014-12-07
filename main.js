66adad413848506099c9c59657c40b6ee0c0b01f
John Jones <johnjones4@gmail.com>
2014-12-07 09:07:47 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+(function($) {
+    $(document).ready(function() {
+        $.jScrollability([
+            {
+                'selector': '.slide-in-demo',
+                'start': 'parent',
+                'end': 'parent',
+                'fn': {
+                    'left': {
+                        'start': 100,
+                        'end': 0,
+                        'unit': '%'
+                    }
+                }
+            },
+            {
+                'selector': '.reveal-demo',
+                'start': 'parent',
+                'end': 'parent',
+                'fn': {
+                    'top': {
+                        'start': 100,
+                        'end': 20,
+                        'unit': '%'
+                    }
+                }
+            },
+            {
+                'selector': '.text-wrapper',
+                'start': function($el) { return $el.offset().top + $el.height() },
+                'end': 'parent',
+                'fn': function($el,pcnt) {
+                    var $spans = $el.find('span');
+                    var point = Math.floor(($spans.length+1) * pcnt);
+                    $spans.each(function(i,el) {
+                        var $span = $(el);
+                        if (i < point) {
+                            $span.addClass('visible');
+                        } else {
+                            $span.removeClass('visible');
+                        }
+                    });
+                }
+            }
+        ]);
+    });
+})(jQuery);
